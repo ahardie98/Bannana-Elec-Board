5116929caa26d5371dbccb5a9204d862478bd468
Jacqui-O <jacqui.oquendo@gmail.com>
2016-03-27 18:11:32 -0500

    Update nrfReceiver.ino

--- nrfReceiver.ino
+++ nrfReceiver.ino
@@ -1,4 +1,4 @@
-/* 1 - GND 
+  /* 1 - GND 
    2 - VCC 3.3V 
    WARNING SHOULD BE 3.3v Not 5V 
    3 - CE - Arduino pin 9
@@ -29,12 +29,22 @@ RF24 radio(CE_PIN, CSN_PIN); // Activate the Radio
 /*-----Declaration of Variables -----*/
  
 int joystick[2];  //  Two element array holding the Joystick readings
- 
+int servoPin = 8; //
+int pulseWidth = 0;
+long lastPulse = 0;    // the time in millisecs of the last pulse
+int refreshTime = 20;  // the time in millisecs needed in between pulses
+int minPulse = 700;   // minimum pulse width
+
 void setup()  
 {
+  pinMode(servoPin, OUTPUT);
+  pulseWidth = minPulse;
+  digitalWrite(servoPin, HIGH);
+  
   Serial.begin(9600); /* Opening the Serial Communication */
   delay(1000);
   Serial.println("Nrf24L01 Receiver Starting");
+  
   radio.begin();
   radio.openReadingPipe(1,pipe);
   radio.startListening();;
@@ -51,15 +61,26 @@ void loop()
     {
       // Fetching the data payload
       done = radio.read( joystick, sizeof(joystick) );
-      Serial.print("X = ");
-      Serial.print(joystick[0]);
-      Serial.print(" Y = ");      
-      Serial.println(joystick[1]);
-    }
+     // if(joystic[1]
+     // Serial.print("X = ");
+     // Serial.print(joystick[0]);
+     pulseWidth = joystick[1] + minPulse;
+      
+     Serial.print(" Moving servo to: ");      
+     // Serial.println(joystick[1]);
+     Serial.println(pulseWidth, DEC);
+     updateServo(); //update servo position
+    } 
+  } //updateServo() here; might need to make this an if statement instead of while loop
+  
+}
+
+void updateServo() {
+  // pulse the servo again if rhe refresh time (20 ms) have passed:
+  if (millis() - lastPulse >= refreshTime) {
+    digitalWrite(servoPin, HIGH);   // Turn the motor on
+    delayMicroseconds(pulseWidth);  // Length of the pulse sets the motor position
+    digitalWrite(servoPin, LOW);    // Turn the motor off
+    lastPulse = millis();           // save the time of the last pulse
   }
-  else
-  {    
-      Serial.println("No radio available");
-  }
- 
 }
