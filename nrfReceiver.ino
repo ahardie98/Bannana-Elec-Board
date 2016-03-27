f84b968d7acd9fce8f58ff793f5b39934edf1e30
Jacqui-O <jacqui.oquendo@gmail.com>
2016-03-27 16:40:17 -0500

    nrfReceiver
    This is the receiving end. Used with Arduino Nano.

---
+++ nrfReceiver.ino
@@ -0,0 +1,65 @@
+/* 1 - GND 
+   2 - VCC 3.3V 
+   WARNING SHOULD BE 3.3v Not 5V 
+   3 - CE - Arduino pin 9
+   4 - CSN - Arduino pin 10
+   5 - SCK - Arduino pin 13
+   6 - MOSI - Arduino pin 11
+   7 - MISO - Arduino pin 12
+   8 - UNUSED */
+ 
+/*----- Import all required Libraries -----*/
+ 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+/*----- Declare all constant pin to be used ----*/
+ 
+#define CE_PIN   9
+#define CSN_PIN 10
+ 
+const uint64_t pipe = 0xE8E8F0F0E1LL; // This is the transmit pipe to communicate the two module
+ 
+ 
+/*-----Object Declaration ----*/
+ 
+RF24 radio(CE_PIN, CSN_PIN); // Activate the Radio
+ 
+/*-----Declaration of Variables -----*/
+ 
+int joystick[2];  //  Two element array holding the Joystick readings
+ 
+void setup()  
+{
+  Serial.begin(9600); /* Opening the Serial Communication */
+  delay(1000);
+  Serial.println("Nrf24L01 Receiver Starting");
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();;
+}//--(end setup )---
+ 
+ 
+void loop()   
+{
+  if ( radio.available() )
+  {
+    // Reading the data payload until the RX received everything
+    bool done = false;
+    while (!done)
+    {
+      // Fetching the data payload
+      done = radio.read( joystick, sizeof(joystick) );
+      Serial.print("X = ");
+      Serial.print(joystick[0]);
+      Serial.print(" Y = ");      
+      Serial.println(joystick[1]);
+    }
+  }
+  else
+  {    
+      Serial.println("No radio available");
+  }
+ 
+}
