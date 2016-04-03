84ad48f6d329c8b4f47495ab35e41967de5c9639
Jacqui-O <jacqui.oquendo@gmail.com>
2016-04-03 15:39:07 -0500

    Update nrfReceiver.ino

--- nrfReceiver.ino
+++ nrfReceiver.ino
@@ -1,4 +1,4 @@
-  /* 1 - GND 
+ /* 1 - GND 
    2 - VCC 3.3V 
    WARNING SHOULD BE 3.3v Not 5V 
    3 - CE - Arduino pin 9
@@ -69,49 +69,48 @@ void loop()
       {  
         // If joystick is greater than center
         pulseWidth = joystick[1] + minPulse;
-        //prev = pulseWidth; 
            
-          if(pulseWidth < prev)        // 1200(pulseWidth) < 1300(prev)
+          if(pulseWidth < prev)        
           {  
-             if(prev != pulseWidth)    // 1300(prev) != 1200(new value)
+             if(prev != pulseWidth)    
              {       
                 prev--;   // decrement previous, until it equals the new value
-                Serial.println("Deccelerating."); // For testing
+               // Serial.println("Deccelerating."); // For testing
                 // Deccelerate
               }
            }
         
-          else if(pulseWidth > prev)      // 1000 > 800
+          else if(pulseWidth > prev)      
           {  
-              if(prev != pulseWidth)      //800 != 1000
+              if(prev != pulseWidth)      
               {  
                   prev++; // increment previous until it equals the new value
-                  Serial.println("Accelerating."); 
-                 // updateServo();
+                  //Serial.println("Accelerating."); 
                   // Accelerate
               }
           }
 
            else 
            {
-                Serial.println("Values not changing");
-                // For when you are at a steady rate
-           }
-                         
-             
-          //updateServo();
+               // Serial.println("Neither");
+                // Neither
+           }                       
       } // End if()
 
-     else if (joystick[1] < 328){  //328
-        pulseWidth = joystick[1] + minPulse;
-      } //to make sure it stops at center and below, works but stops suddenly, also missing else
-    //  else
-    //  {
-       
-     //  }
-//Serial.println(prev); //
+     else if (joystick[1] < 329) //1300 to 700
+      {  
+        pulseWidth = 700; //or ==
+        if(prev != pulseWidth)
+        {
+          prev--;
+          //Serial.println("Stopping"); 
+         // Serial.print(prev); 
+        //decrement previous until it is 700
+        //pulseWidth = 700; //Do not move
+        } //pulseWidth = 700;
+      } 
       updateServo(); //update servo position, works here 
-    //  Serial.println(prev); doesnt stop!
+      Serial.println(pulseWidth);
     } //End while ()
   } // End if()
 } // End loop()
@@ -120,7 +119,8 @@ void updateServo() {
   // pulse the servo again if rhe refresh time (20 ms) have passed:
   if (millis() - lastPulse >= refreshTime) {
     digitalWrite(servoPin, HIGH);   // Turn the motor on
-    delayMicroseconds(pulseWidth);        // Length of the pulse sets the motor position
+    delayMicroseconds(prev);        // Length of the pulse sets the motor position
+    // need prev here? how is it affecting deccel accel?
     digitalWrite(servoPin, LOW);    // Turn the motor off
     lastPulse = millis();           // save the time of the last pulse
   }
